df0769c8835e8756007261cf219667fee3fc501c
JuanCasih <jccasih@gmail.com>
2024-04-20 12:46:32 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("background-image").addEventListener("click", function() {
+    window.location.href = "pages/desarollo.html"
+  });
+  document.getElementById("background-image2").addEventListener("click", function() {
+    window.location.href = "pages/diseño.html"
+  });
+  document.getElementById("background-image3").addEventListener("click", function() {
+    window.location.href = "pages/ecommerce.html"
+  });
+  document.getElementById("audiovisual").addEventListener("click", function() {
+    window.location.href = "pages/audiovisual.html"
+  });
+  document.getElementById("background-image5").addEventListener("click", function() {
+    window.location.href = "pages/3D.html"
+  });
+  document.getElementById("background-image6").addEventListener("click", function() {
+    window.location.href = "pages/branding.html"
+  });
+
+  
